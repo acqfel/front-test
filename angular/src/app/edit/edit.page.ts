d3b1005c829da401781c3db45fc008a62825e95e
Felipe <unomais@yahoo.com.br>
2018-10-08 17:17:12 -0300

    ang Added: edit component - editable list.

--- angular/src/app/edit/edit.page.ts
+++ angular/src/app/edit/edit.page.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { legends } from '../names/nameslist';
 
 @Component({
   selector: 'app-edit',
   templateUrl: 'edit.page.html',
   styleUrls: ['edit.page.scss']
 })
-export class EditPage {
+export class EditPage implements OnInit {
+  
+  legends: Array<string> = legends;
+  editMode: Array<boolean> = [];
+  
+  inputVal: string;
 
-  constructor() {
-    
+  constructor() { 
+  }
+  
+  ngOnInit() {
+    for (let i = 0; i < this.legends.length; i++) {
+      this.editMode.push(false);
+    }
+  }
+  
+  save(legend, index){
+    legends[index] = legend;
+    this.editMode[index] = false;
+  }
+  
+  edit(index) {
+    this.editMode[index] = true;
   }
 
 }
